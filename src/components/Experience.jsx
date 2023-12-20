dfce74163c5ff64e57d3f7e911ec1abbe88f85f1
KshitijPatel7 <KshitijPatel7@users.noreply.github.com>
2023-12-20 12:42:25 +0530

    updated experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -64,7 +64,7 @@ const Experience = () => {
           What I have done so far
         </p>
         <h2 className={`${styles.sectionHeadText} text-center`}>
-          Work Experience.
+          Academic Background.
         </h2>
       </motion.div>
 
